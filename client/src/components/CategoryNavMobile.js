79bf2ddcb6ca7c9f5465382e97f0098b97c30c61
Aquib Iqbal <aquibiqbal65@gmail.com>
2024-07-08 19:44:29 +0530

    complete frontend

--- client/src/components/CategoryNavMobile.js
+++ client/src/components/CategoryNavMobile.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import React from "react";
+import { FiX } from "react-icons/fi";
+import { Link } from "react-router-dom";
+import useFetch from "../hooks/useFetch";
 
-const CategoryNavMobile = () => {
-  return <div>CategoryNavMobile</div>;
+const CategoryNavMobile = ({ setCatnavMobile }) => {
+  const { data } = useFetch("/categories");
+  return (
+    <div className="w-full h-full bg-primary p-8 ">
+      <div
+        onClick={() => setCatnavMobile(false)}
+        className="flex justify-end mb-8 cursor-pointer"
+      >
+        <FiX className="text-3xl" />
+      </div>
+      <div className="flex flex-col gap-y-8">
+        {data?.map((category) => {
+          return (
+            <Link to={`products/${category.id}`} className="uppercase font-medium"
+             key={category.div}>
+              {category.attributes.title} Cameras
+            </Link>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default CategoryNavMobile;
--- client/src/components/SearchForm.js
+++ client/src/components/SearchForm.js
@@ -1,7 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 
+import { FiSearch } from "react-icons/fi";
+import { useNavigate } from "react-router-dom";
 const SearchForm = () => {
-  return <div>SearchForm</div>;
+  const navigate = useNavigate();
+  const [searchTerm, setSearchTerm] = useState("");
+  const [isAnimating, setIsAnimating] = useState("false");
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setIsAnimating(false);
+    }, 1000);
+    return () => clearTimeout(timeout);
+  })
+
+  const handleSearchInput = (e) => {
+    setSearchTerm(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (searchTerm.length > 0) {
+      navigate(`/search?query=${searchTerm}`);
+      document.querySelector("input").value = "";
+      setSearchTerm("");
+    } else {
+      setIsAnimating(true);
+    }
+  };
+  return (
+    <form onSubmit={handleSubmit} className={`${isAnimating ? 'animate-shake' : 'animate-none'} w-full h-auto relative`}>
+      <input
+        onChange={handleSearchInput}
+        className="input"
+        type="text"
+        placeholder="Search for a product..."
+      />
+      <button className="btn btn-accent absolute top-0 right-0 rounded-l-none">
+        <FiSearch className="text-xl" />
+      </button>
+    </form>
+  );
 };
 
 export default SearchForm;
